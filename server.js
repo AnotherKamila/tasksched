6454fc429f5a81fe39361677be5d2e50a8ea3a39
Evilham <github@evilham.com>
2018-03-01 11:08:46 +0100

    Resolves #25 by reading the active filter from TW

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ const PORT          = process.env.PORT || 5000
 const TASK          = 'task'
 const TASKOPTS      = ['rc.gc=off']
 const DEVSERVER     = 'http://localhost:8000' // Proxies this from /dev (and allows CORS)
-const ALWAYS_FILTER = '(+PENDING or (+WAITING +SCHEDULED))'
 
 const api = express()
 api.use(body_parser.json())
 api.use(cors({origin: DEVSERVER}))
 
 api.get('/tasks', (req, res) => {
-    export_tasks(req.query.filter, {
+    export_filtered_tasks(req.query.filter, {
         on_data: (chunk) => res.write(chunk),
         on_exit: (c, s)  => res.end(c || s ? format_status(c, s) : '')
     })
@@ -42,9 +41,55 @@ server.use('/dev', proxy(DEVSERVER))
 server.listen(PORT, () => console.log(`listening on port ${PORT}`))
 
 
+const read_active_filter = ({on_filter}) => {
+    // Step 1: See if there is an active context
+    const tw_context = readline.createInterface({
+        input: spawn_tw(['_get', 'rc.context']).stdout
+    })
+    let context = ''
+    tw_context.on('line', (line) => {
+        if (!context && line) context = line
+    })
+    tw_context.on('close', (line) => {
+        // Step 2: If there is an active context, get its filter.
+        //         If not, get the filter for the "next" report.
+        let cmd = ['_get']
+        if (context)
+            cmd.push('rc.context.' + context)
+        else
+            cmd.push('rc.report.next.filter')
+
+        const tw_filter = readline.createInterface({
+            input: spawn_tw(cmd).stdout
+        })
+
+        let filter = ''
+        tw_filter.on('line', (line) => {
+            if (!filter && line) filter = line
+        })
+        tw_filter.on('close', (line) => {
+            // Execute callback with obtained filter
+            on_filter(filter)
+        })
+    })
+}
+
+
+const export_filtered_tasks = (filter, callbacks) => {
+    // If a filter is already passed, don't bother checking taskwarrior's
+    if (filter)
+        return export_tasks(filter, callbacks)
+
+    read_active_filter({on_filter: (f) => export_tasks(f, callbacks)})
+}
+
+
 const export_tasks = (filter, {on_data, on_exit}) => {
-    const f = `${ALWAYS_FILTER} ${filter || ''}`
-    const tw = spawn(TASK, TASKOPTS.concat(['rc.json.array=on', f, 'export']))
+    let cmd = ['rc.json.array=on']
+    if (filter)
+        cmd.push(filter)
+    cmd.push('export')
+    const tw = spawn_tw(cmd)
     tw.stdout.on('data', on_data)
     tw.on('exit', on_exit)
 }
@@ -66,7 +111,7 @@ const import_task = ({command, task}, {on_exit}) => {
         for (var attr in task) if (task.hasOwnProperty(attr) && attr != 'uuid') cmd.push(attr+':'+(task[attr] || ''))
     }
 
-    const tw = spawn(TASK, TASKOPTS.concat(cmd))
+    const tw = spawn_tw(cmd)
     console.log("[import] running command: task " + cmd.join(' '))
     tw.on('exit', on_exit)
 }
@@ -74,7 +119,7 @@ const import_task = ({command, task}, {on_exit}) => {
 const format_status = (code, status) => `{"status": "${status || code || ''}"}`
 
 const check_timew = ({on_exit}) => {
-    const tw = spawn(TASK, TASKOPTS.concat(['diagnostics']))
+    const tw = spawn_tw(['diagnostics'])
     const rl = readline.createInterface({
         input: tw.stdout
     })
@@ -89,6 +134,10 @@ const check_timew = ({on_exit}) => {
     })
 
     rl.on('close', (line) => {
-	if (!response_sent) on_exit(false)
+        if (!response_sent) on_exit(false)
     })
+}
+
+const spawn_tw = (opts) => {
+    return spawn(TASK, TASKOPTS.concat(opts))
 }
