0fb70f6782085c182c9e0adaf2c8b6f4aa6cf6ba
Evilham <github@evilham.com>
2018-11-20 19:06:56 +0100

    First attempt at parsing filter from url.

--- src/Model.elm
+++ src/Model.elm
@@ -10,6 +10,10 @@ import Html5.DragDrop as DragDrop
 import Taskwarrior.Model as Taskwarrior
 import Utils.Date
 
+type alias UrlState =
+    { next : Bool
+    , filter: String
+    }
 
 type alias Model =
     { tasks    : List Taskwarrior.Task
@@ -17,6 +21,7 @@ type alias Model =
     , now      : Date.Date
     , err      : String
     , url      : Navigation.Location
+    , urlState : UrlState
     , timew    : Bool
     -- Boilerplate
     , dragDrop : DragDrop.Model Dragged DroppedOnto
@@ -42,6 +47,27 @@ type alias Dragged     = Taskwarrior.Task
 type alias DroppedOnto = Maybe Date.Date -- Maybe because we can also unschedule
 
 
+parse_hash : String -> UrlState
+parse_hash hash =
+    let next         = (String.startsWith "#next" hash)
+        filter_parts = (Maybe.withDefault []
+                            (List.tail (String.split "?" hash)))
+        filter = (String.join "?" filter_parts)
+    in
+      UrlState next filter
+
+put_hash : Model -> Cmd msg
+put_hash model =
+    let urlState = model.urlState
+        hash = String.join ""
+               [ "#"
+               , (if urlState.next then "next" else "")
+               , (if String.isEmpty urlState.filter then "" else "?")
+               , urlState.filter
+               ]
+    in
+        Navigation.modifyUrl hash
+
 -- INIT --
 
 init : Navigation.Location -> Model
@@ -51,6 +77,7 @@ init location =
     , now      = Utils.Date.date_0
     , err      = ""
     , url      = location
+    , urlState = parse_hash location.hash
     , timew    = False
     -- Boilerplate
     , dragDrop = DragDrop.init
